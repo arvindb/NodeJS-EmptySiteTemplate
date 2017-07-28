6c337d372187b7c744265a02fd7215a71a787849
arvindb <arvindbhambhani@gmail.com>
2017-07-28 20:06:36 +0530

    Add files via upload
    mongo db added

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var http = require('http');
 
-var mongo = require('mongodb');
 
 http.createServer(function (req, res) {
     
@@ -9,4 +8,16 @@ http.createServer(function (req, res) {
     
 }).listen(process.env.PORT || 8080);
 
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://localhost:27017/testdb';
 
+MongoClient.connect(url, function(err, db) {
+
+    var cursor = db.collection('testdb').find();
+
+    cursor.each(function(err, doc) {
+
+        console.log(doc);
+
+    });
+}); 

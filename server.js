7d6450f3c27b0a86fd387a8ebddb32cc920d95a1
arvindb <arvindbhambhani@gmail.com>
2017-07-28 20:22:26 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -7,15 +7,17 @@ http.createServer(function (req, res) {
     
     var mydata  = "hello world start, ";
 
-    var MongoClient = require('mongodbx').MongoClient;
+    var MongoClient = require('mongodb').MongoClient;
     var url = "mongodb://localhost:27017/mydb";
+
 
     MongoClient.connect(url, function(err, db) {
     if (err) throw err;
-    mydata = "Database created!";
+    mydata = mydata + "Database created!";
     db.close();
     });
 
+    
 
     mydata = mydata  + "hello world end";
 

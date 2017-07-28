a44df1ece14ac511bee5b6f5a68535c03446c749
arvindb <arvindbhambhani@gmail.com>
2017-07-28 20:18:54 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,32 +1,30 @@
 var http = require('http');
 
 
- var MongoClient = require('mongodb').MongoClient;
- var url = 'mongodb://localhost:27017/testdb';
-
-
 http.createServer(function (req, res) {
     
     res.writeHead(200, { 'Content-Type': 'text/html' });
     
     var mydata  = "hello world start, ";
 
-   
+    var MongoClient = require('mongodb').MongoClient;
+    var url = "mongodb://localhost:27017/mydb";
+
     MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    mydata = "Database created!";
+    db.close();
+    });
 
-        var cursor = db.collection('testdb').find();
 
-        cursor.each(function(err, doc) {
-
-            mydata = mydata  + "connection opened, ";
-
-        });
-    }); 
-
-mydata = mydata  + "hello world end";
+    mydata = mydata  + "hello world end";
 
     res.end(mydata);
     
 }).listen(process.env.PORT || 8080);
+
+
+
+
 
 
